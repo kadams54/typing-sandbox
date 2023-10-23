41987bb7d0f8fdf2c2821a584dbe8f622bbdc8bc
Kyle Adams <kadams54@users.noreply.github.com>
2023-10-23 19:11:29 -0400

    Add dataclasses implementation

---
+++ typing_sandbox/dataclasses.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from typing import Optional, Sequence
+
+
+@dataclass
+class Cat:
+    age: int
+    name: Optional[str]
+
+
+@dataclass
+class StrictCat(Cat):
+    name: str
+
+
+def echo(cats: Sequence[Cat]) -> Sequence[Cat]:
+    return cats
+
+
+def name(cats: Sequence[Cat]) -> Sequence[str]:
+    return [c.name or "" for c in cats]
+
+
+cats: Sequence[StrictCat] = [
+    StrictCat(age=4, name="Daisy"),
+    StrictCat(age=6, name="Superman"),
+]
+other_cats = echo(cats)
+print(name(other_cats))
