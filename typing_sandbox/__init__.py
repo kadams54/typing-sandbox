de60e76f6de009c69513af27b0b5d9ad17274846
Kyle Adams <kadams54@users.noreply.github.com>
2023-10-23 18:53:29 -0400

    Push harder on the typing

--- typing_sandbox/__init__.py
+++ typing_sandbox/__init__.py
@@ -14,8 +14,13 @@ def echo(cats: list[Cat]) -> list[Cat]:
     return cats
 
 
+def name(cats: list[StrictCat]) -> list[str]:
+    return [c["name"] for c in cats]
+
+
 cats: list[StrictCat] = [
     {"age": 4, "name": "Daisy"},
     {"age": 6, "name": "Superman"},
 ]
 other_cats = echo(cats)
+print(name(other_cats))
