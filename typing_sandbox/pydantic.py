44b16fb77058ec4f6277f83a30619209ea126927
Kyle Adams <kadams54@users.noreply.github.com>
2023-10-23 23:27:15 -0400

    Add Pydantic implementation

---
+++ typing_sandbox/pydantic.py
@@ -0,0 +1,30 @@
+from typing import Optional, Sequence
+
+from pydantic import BaseModel, Field
+
+
+class Cat(BaseModel):
+    age: int = Field(ge=0, lt=50)
+    name: Optional[str]
+
+
+class StrictCat(Cat):
+    name: str
+
+
+def echo(cats: Sequence[Cat]) -> Sequence[Cat]:
+    return cats
+
+
+def name(cats: Sequence[Cat]) -> Sequence[str]:
+    return [c.name or "" for c in cats]
+
+
+def run():
+    cats: Sequence[StrictCat] = [
+        StrictCat(age=4, name="Daisy"),
+        StrictCat(age=6, name="Superman"),
+    ]
+    other_cats = echo(cats)
+    print(name(other_cats))
+    print(Cat.model_json_schema())
