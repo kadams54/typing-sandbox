da48bb5ae732af90ba9115e47e0b29131b3111bb
Kyle Adams <kadams54@users.noreply.github.com>
2023-10-25 10:14:02 -0400

    Compare Pydantic with an io.ts example

---
+++ typing_sandbox/pydantic_iots.py
@@ -0,0 +1,29 @@
+from typing import Any
+
+from pydantic import BaseModel, ValidationError
+from rich import print
+
+
+# const User = t.type({
+#     userId: t.number,
+#     name: t.string,
+# });
+class User(BaseModel):
+    user_id: int
+    name: str
+
+
+def run(data: Any) -> None:
+    # const decoded = User.decode(data); // Either<Errors, User>
+    try:
+        decodedUser = User(**data)
+    # if (isLeft(decoded)) {
+    #   throw Error(
+    #     `Could not validate data: ${PathReporter.report(decoded).join("\n")}`
+    #   );
+    #   // e.g.: Could not validate data: Invalid value "foo" supplied to : { userId: number, name: string }/userId: number
+    #
+    except ValidationError as error:
+        raise Exception(f"Could not validate data: {error.errors}")
+    print(f"User: {decodedUser}")
+    print(f"User type: {type(decodedUser)}")
